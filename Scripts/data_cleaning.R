d4032dc53948b0209c0761c04b85ce9403e366a4
paolocrosetto <paolo.crosetto@gmail.com>
2022-09-20 16:16:12 +0200

    adding individual-level variable support to the datasets

--- Scripts/data_cleaning.R
+++ Scripts/data_cleaning.R
@@ -52,12 +52,61 @@ summarised <- summarised %>% left_join(wordtask, by="subjectID")
 ##cleaning
 rm(choicesets,groups, treatment, wordtask)
 
+
+
+## computing skills, luck, greed, royalty aversion of individuals
+
+
+#indicator of **luck**
+luck <- actions %>% 
+  select(subjectID, repetition, period, meancs) %>% 
+  filter(period == 1) %>% 
+  group_by(repetition) %>% 
+  select(subjectID, init_potential = meancs) %>% 
+  mutate(luck = 100*init_potential/max(init_potential, na.rm = T)) %>%  #make the indicator relative to best, 0-100
+  select(subjectID, luck)
+
+# indicator of **royalty_aversion**
+roy_aversion <- actions %>% 
+  mutate(extension = str_length(letters_added) != 3) %>% 
+  filter(extension == TRUE) %>% 
+  select(subjectID, repetition, period, payoff, royalties_spent) %>% 
+  mutate(roy_spent_share = royalties_spent / (payoff + royalties_spent)) %>% 
+  filter(repetition == 1) %>% 
+  group_by(subjectID) %>% 
+  summarise(royalty_aversion = 100*(1 - mean(roy_spent_share)))
+
+# voting
+
+skill_greed <- summarised %>% 
+  filter(repetition == 1) %>% 
+  mutate(skill = 100*Npoints/max(Npoints, na.rm = T)) %>% 
+  select(subjectID, skill, greed = percasked)
+
+pers_charac <- skill_greed %>% 
+  left_join(roy_aversion) %>% 
+  left_join(luck)
+
+
+## merging with all concerned datasets
+
+summarised <- summarised %>% left_join(pers_charac)
+actions <- actions %>% left_join(pers_charac)
+
+rm(pers_charac, roy_aversion, skill_greed, luck)
+
+
+
 ## group-level data
 grouped <- actions %>% 
-  group_by(groupID, IPregime, votetreat, info, repetition) %>%
+  group_by(groupID, IPregime, vote = votetreat, info, repetition) %>%
   mutate(wordvalue = payoff + royalties_spent,
          roundpayoff = payoff + royalties_earned) %>% 
   summarise(points = sum(roundpayoff, na.rm = T),
+            skills = mean(skill),
+            royaversion = mean(royalty_aversion),
+            luck = mean(luck),
+            greed = mean(greed),
             Nw = sum(!is.na(new_word)),
             Next = sum(str_length(letters_added)!= 3, na.rm = T),
             Nroot = sum(str_length(letters_added)== 3, na.rm = T),
