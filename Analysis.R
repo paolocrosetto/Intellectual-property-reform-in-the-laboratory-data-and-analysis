47feeb220bbe328a16d2cee8bc7fb5d0087afd1f
paolocrosetto <paolo.crosetto@gmail.com>
2022-09-20 16:17:14 +0200

    section 4.1 fully covered

--- Analysis.R
+++ Analysis.R
@@ -8,18 +8,28 @@
 ### Libraries
 library(tidyverse)                ### the specific R dialect used in this script
 library(kableExtra)               ### export beautiful tables
-library(broom)                    ### run efficently statistical tests and export them to data frames
+library(huxtable)                 ### export beautiful tables (alternative package)
+library(broom)                    ### run efficiently statistical tests and export them to data frames
+library(effsize)                  ### computing effect sizes using coehn's d
 
 
-### Single analysis files
+### Analysis scripts
+
+### 0. data cleaning and preparation for the rest of the analysis
+source("Scripts/data_cleaning.R")
+
 
 ### 1. sample demographics
 source("Scripts/sample_statistics.R")
 
 
-### 2. data cleaning and preparation for the rest of the analysis
-source("Scripts/data_cleaning.R")
-
 ### 2. Effects of abolishing intellectual property
 
+# group statistics
+source("Scripts/group_points_transition.R")
 
+# group regression
+source("Scripts/table_2.R")
+
+# individual-level regression
+source("Scripts/individual_points_transition.R")
