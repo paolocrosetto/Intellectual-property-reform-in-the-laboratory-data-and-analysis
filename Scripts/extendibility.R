b24fc0dbf832a19595c2d1a2ce9763bfe82cde42
paolocrosetto <paolo.crosetto@gmail.com>
2022-09-22 11:02:51 +0200

    initial work on extendibility integration

---
+++ Scripts/extendibility.R
@@ -0,0 +1,10 @@
+###
+###
+### This file merges the extendibility data with actions
+###
+### To check the referees' concern that "best" players were actually forward looking
+### i.e., creating roots that had high potential. 
+
+
+### data
+
